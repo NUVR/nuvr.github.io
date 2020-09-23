3ba5b71f6014e2da6a75fab414293371eb6e182d
Peter Abbondanzo <peter@abbondanzo.com>
2020-09-23 15:56:04 -0400

    Add more slides (#9)
    * Add more photos as slides
    
    * Increase cycle time
    
    * Add slides and update about page images

--- src/assets/scripts/slideshow.js
+++ src/assets/scripts/slideshow.js
@@ -1,32 +1,33 @@
 let slideIndex = 0;
 const ANIMATION_TIME = 1500;
-const CYCLE_TIME = 5000;
-const ELEMENTS = document.getElementsByClassName('slide');
+const CYCLE_TIME = 8000;
+const ELEMENTS = document.getElementsByClassName("slide");
 
 const carousel = () => {
-    const currentElement = ELEMENTS[slideIndex];
-    const nextElement = ELEMENTS[slideIndex + 1 >= ELEMENTS.length ? 0 : slideIndex + 1];
+  const currentElement = ELEMENTS[slideIndex];
+  const nextElement =
+    ELEMENTS[slideIndex + 1 >= ELEMENTS.length ? 0 : slideIndex + 1];
 
-    setTimeout(() => {
-        addFade(nextElement);
-    }, CYCLE_TIME - ANIMATION_TIME);
-    setTimeout(() => {
-        removeFade(currentElement);
-    }, CYCLE_TIME);
+  setTimeout(() => {
+    addFade(nextElement);
+  }, CYCLE_TIME - ANIMATION_TIME);
+  setTimeout(() => {
+    removeFade(currentElement);
+  }, CYCLE_TIME);
 
-    slideIndex++;
-    if (slideIndex >= ELEMENTS.length) {
-        slideIndex = 0;
-    }
-    setTimeout(carousel, CYCLE_TIME);
+  slideIndex++;
+  if (slideIndex >= ELEMENTS.length) {
+    slideIndex = 0;
+  }
+  setTimeout(carousel, CYCLE_TIME);
 };
 
-const addFade = element => {
-    element.classList.add('fade');
+const addFade = (element) => {
+  element.classList.add("fade");
 };
 
-const removeFade = element => {
-    element.classList.remove('fade');
+const removeFade = (element) => {
+  element.classList.remove("fade");
 };
 
 carousel();
