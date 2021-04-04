7794b0dbb8624c2c4658fab753380a96872b7269
CircleCI <circleci@nuvr.io>
2021-04-04 03:57:47 +0000

    Update site (2021-04-04 03:57:47 UTC) [ci skip]

---
+++ assets/scripts/console.js
@@ -0,0 +1,14 @@
+function log() {
+    console.log("\
+8   8       8 8          88888 8                      8 \n\
+8www8 .d88b 8 8 .d8b.      8   8d8b. .d88b 8d8b .d88b 8 \n\
+8   8 8.dP' 8 8 8' .8      8   8P Y8 8.dP' 8P   8.dP' ' \n\
+8   8 `Y88P 8 8 `Y8P'      8   8   8 `Y88P 8    `Y88P w \n\
+\n\
+If we were a company, this would be a great place to give you a link to our job openings. But we're not. You can still apply anyways if you're inspired to.\n\
+\n\
+https://nuvr.github.io/join\
+")
+}
+
+window.addEventListener("load", log);
